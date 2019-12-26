f0dbd6d6a8cda0eaf1365c65f049c5c0f82872f6
BZ <bardichezanber@gmail.com>
2019-12-26 15:24:06 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import Head from '../components/head'
 import Nav from '../components/nav'
 
-const Home = () => {
+const Home = (props) => {
   const [date, setDate] = useState(null);
 
   useEffect(() => {
@@ -21,7 +21,7 @@ const Home = () => {
       <Nav />
 
       <div className="hero">
-        <h1 className="title">Welcome to Next!</h1>
+        <h1 className="title">Welcome to {props.host}!</h1>
         <p className="description">
           To get started, edit the <code>pages/index.js</code> or <code>pages/api/date.js</code> files, then save to reload.
         </p>
